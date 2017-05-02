f6f918b46ed7c783dd7748b03d5b59367b34fb0f
Siobhan <dunphyse@gmail.com>
2017-05-02 14:51:06 +0100

    Created pandas dataframe in changes.py using class Commit and to_dict function.

--- CA4/changes.py
+++ CA4/changes.py
@@ -7,6 +7,8 @@
 # Your statistical analytics conclusions should be in a word document explaining in approximately 500 words the information that you have gleamed from the dataset.
 # You will be required to submit your code via github along with all documentation and tests.
 
+import pandas as pd
+
 # Open file and read in the lines.
 # 
 changes_file = 'changes_python.log'
@@ -14,8 +16,6 @@ changes_file = 'changes_python.log'
 # Use strip to strip out spaces.
 my_file = open(changes_file, 'r')
 data = my_file.readlines()
-
-print data
 
 data = [line.strip() for line in open(changes_file, 'r')]
 
@@ -28,20 +28,22 @@ sep = 72*'-'
 # Create a class to store the elements of each object
 class Commit(object):
     def __init__(self, revision=None, author=None, date=None, comment_line_length=None, 
-            changes=None, comment=None):
+            type_of_changes=None, comment=None):
         self.revision = revision
         self.author = author
         self.date = date
         self.comment_line_length = comment_line_length
-        self.changes = changes
-        self.comment = comment
+        self.type_of_changes = type_of_changes
+        self.changes = comment
 
-a_commit = Commit('r1551925', 'Thomas', '2015/11/27', 1, None, 
-        'Renamed folder to the correct name')
-
-print a_commit.author
-print a_commit.comment
-
+    def to_dict(self):
+        return {'revision': self.revision,
+                'author': self.author,
+                'date': self.date,
+                'type of change': self.type_of_changes,
+                'changes': self.changes
+                }
+        
 index = 0
 current_commit = None
 commits = []
@@ -49,20 +51,20 @@ commits = []
 while True:
     try:
         details = data[index+1].split('|')
-        current_commit =Commit()
+        current_commit = Commit()
         current_commit.revision = details[0].strip()
         current_commit.author = details[1].strip()
         current_commit.date = details[2].strip()
         current_commit.comment_line_length = int(details[3].strip().split(' ')[0])
-        # print current_commit.revision
-        current_commit.changes = data[index+2:data.index('',index+1)]
-        index = data.index(sep, index+1)
         
+        index = data.index(sep, index+1)
         current_commit.changes = data[index-current_commit.comment_line_length:index]
-        print current_commit.changes
         
         commits.append(current_commit)
     except IndexError:
         break
 
 print len(commits)
+# Create data frame
+df = pd.DataFrame(commit.to_dict() for commit in commits)
+print df.head(20)
